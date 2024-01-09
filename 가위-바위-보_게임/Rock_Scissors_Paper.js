3a3c88570a1a10188665d9814514766d08e3331d
shin <minchae.shin1@gmail.com>
2024-01-09 22:46:52 +0900

    js소스코드분해해서공부

--- 가위-바위-보_게임/Rock_Scissors_Paper.js
+++ 가위-바위-보_게임/Rock_Scissors_Paper.js
@@ -8,6 +8,13 @@ let computer_score=document.getElementById("computer_score");
 let reset_btn = document.getElementById("reset_btn");
 let end_btn = document.getElementById("end_btn");
 
+//document.getElementById는 DOM(Document object Model)에서
+//특정 ID를 가진 HTML 앨리먼트를 찾아서 반환하는 메서드
+//즉, HTML엘리먼트를 javascript참조하고자 할때 사용
+//예를들어, 이미지 엘리먼트의 속성을 변경하거나 버튼 클릭 이벤트를 추가등의 작업을 할때 할당된 변수 활용
+//엘리먼트 = HTML문서의 각각의 구성요소를 가리키는 용어
+
+//이미지 경로를 저장하는 배열
 let imgArray= new Array();
 imgArray[0]="C:/Users/shinpardon/IdeaProjects/Practice_JS/Practice_JS/가위-바위-보_게임/img/paper.jpg";
 imgArray[1]="C:/Users/shinpardon/IdeaProjects/Practice_JS/Practice_JS/가위-바위-보_게임/img/rock.jpg";
@@ -15,30 +22,35 @@ imgArray[2]="C:/Users/shinpardon/IdeaProjects/Practice_JS/Practice_JS/가위-바
 
 
 let a;
-let userscore=0;
-let computerscore=0;
+let userscore=0;  // 사용자 점수 초기화
+let computerscore=0; // 컴퓨터 점수 초기화
 
+//이미지를 무작위로 변경하는 함수
 function imagetime()
 {
-    a = Math.floor(Math.random()*3);
-    img.src=imgArray[a];
+    a = Math.floor(Math.random()*3); // 0, 1, 2 중 하나의 무작위 정수 생성
+    img.src=imgArray[a]; // 무작위로 선택된 이미지 경로는 img 엘리먼트의 src 속성에 할당
     
-    //console.log(a);
+    //console.log(a); -> 선택된 이미지의 인덱스를 콘솔에 출력
 }
 
-setInterval(imagetime,150);
+setInterval(imagetime,150); //imagetime함수를 150밀리초 간격으로 반복 실행하는 코드
+
+//imagetime이라는 함수 정의를 하고, 이 함수를 150밀리초마다 실행
+//setInterval함수 사용
 
 
 
 //가위를 클릭한 경우 
 scissors.onclick = ()=>{
-    clearInterval();
+    clearInterval(); //이미지 변경 멈춤
     //승리하는 경우(컴퓨터 : 보를 낸 경우 )
     if(a==0)
     {
         alert("승리");
         userscore++;
-        user_score.textContent=userscore;
+        user_score.textContent=userscore; // 사용자 점수 업데이트
+        // textContent 텍스트 콘텐츠를 가져옴
     }
     else if(a==2) //(컴퓨터 : 가위 낸 경우)
     {
@@ -48,25 +60,25 @@ scissors.onclick = ()=>{
     {
         alert("패배");
         computerscore++;
-        computer_score.textContent=computerscore;        
+        computer_score.textContent=computerscore;  // 사용자 점수 업데이트      
     }
     
 }
 
 //바위를 클릭한 경우
 rock.onclick= () =>{
-    clearInterval();
+    clearInterval(); //이미지 변경 멈춤
     if(a==0)// (컴퓨터 : 보를 낸 경우)
     {
         alert("패배");
         computerscore++;
-        computer_score.textContent=computerscore;  
+        computer_score.textContent=computerscore;  // 사용자 점수 업데이트
     }
     else if(a==2) //(컴퓨터 : 가위낸 경우)
     {
         alert("이겼습니다.");
         userscore++;
-        user_score.textContent=userscore;
+        user_score.textContent=userscore; // 사용자 점수 업데이트
     }
     else //(컴퓨터 : 묵 낸경우)
     {
@@ -75,7 +87,7 @@ rock.onclick= () =>{
 }
 //보를 클릭한 경우
 paper.onclick=()=>{
-    clearInterval();
+    clearInterval(); //이미지 변경 멈춤
     if(a==0)// (컴퓨터 : 보를 낸 경우)
     {
         alert("비겼습니다");
@@ -84,13 +96,13 @@ paper.onclick=()=>{
     {
         alert("패배.");
         computerscore++;
-        computer_score.textContent=computerscore;  
+        computer_score.textContent=computerscore;  // 사용자 점수 업데이트
     }
     else //(컴퓨터 : 묵 낸경우)
     {
         alert("이겼습니다.");
         userscore++;
-        user_score.textContent=userscore;
+        user_score.textContent=userscore; // 사용자 점수 업데이트
     }
 }
 
@@ -99,7 +111,7 @@ reset_btn.onclick=()=>{
     let check=confirm("다시 시작하시겠습니까?");
     if(check==true)
     {
-        location.reload();
+        location.reload();  // 페이지 새로 고침
     }
 }
 
@@ -109,6 +121,6 @@ end_btn.onclick=()=>{
     if(check==true)
     {
         alert("게임 승리 : " + userscore  + " 게임 패배 : " + computerscore);
-        location.reload();
+        location.reload(); // 페이지 새로 고침
     }
 }
